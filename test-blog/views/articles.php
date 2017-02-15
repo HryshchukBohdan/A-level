e18c459ce02f97fa4c7f0161016c71e7779a1a73
Hryshchuk Bohdan <bohdanuk16@gmail.com>
2017-02-15 20:12:17 +0200

    do_valid

--- test-blog/views/articles.php
+++ test-blog/views/articles.php
@@ -3,11 +3,15 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     </head>
     <body>
         <div class="container">
             <h1> Названия блога</h1>
+            			<div>
+				<a href="add.php" class="btn btn-danger btn-block">Добавить запись</a>
+			</div>
             
 <form action="index.php" method="post" style="float:center">
 <input type="submit" name=btn2 value="Sort">
