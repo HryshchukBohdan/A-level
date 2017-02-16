8f51b64ad334190eac76f49047d1128a8fd14063
Hryshchuk Bohdan <bohdanuk16@gmail.com>
2017-02-16 13:22:52 +0200

    dom_fin

--- lesson_19/poisk_kv.php
+++ lesson_19/poisk_kv.php
@@ -4,11 +4,11 @@ class House
     public $n_kv_na_etaz;
     public $n_etaz_d;
     public $n_kv_poiska;
+    
     public $n_kv_pod;
     public $n_pod;
     public $n_etaz;
     
-    //передача данных из объекта в конструктор
     public function __construct()
     {   
         echo "Vvedit nomer kvartiri poiska:\n";
@@ -21,65 +21,23 @@ class House
 
         echo "Vvedit kolichestvo etazei doma:\n";
         $handle = fopen ("php://stdin","r");
-        $this->n_etaz_d = fgets($handle);
+        $this->n_etaz_d = fgets($handle);   
     }
 
     public function schot() {
         $this->n_kv_pod = $this->n_kv_na_etaz * $this->n_etaz_d;
-        echo $this->n_kv_pod;
+ 
+        $this->n_pod = ceil($this->n_kv_poiska / $this->n_kv_pod);
+
+        $this->n_etaz = ceil(($this->n_kv_poiska - (($this->n_pod - 1) * $this->n_kv_pod)) / $this->n_kv_na_etaz) ;
     }
 
-
-       // $this->type = $type;
-      //  $this->nomer_kvartiri = $this->enterNumber();;
-    
-}
- /*   //ввод номера квартиры пользователем
-    function enterNumber(){
-        echo "Пожалуйста введите номер квартиры:\n";
-        $handle = fopen ("php://stdin","r");
-        return $kv= fgets($handle);
-    }
-    //определение по типу дома, количества этажей и количества квартир на этаже
-    public function type()
-    {
-        if ($this->type == 'SLT_9_7') {
-            $this->kvartir_na_etaje = 4;
-            $this->etajey_v_dome = 9;
-        } elseif ($this->type == 'KHR_5_4') {
-            $this->kvartir_na_etaje = 3;
-            $this->etajey_v_dome = 5;
-        } else echo "unkown";
-    }
-    // вывод типа здания
-    public function start()
-    {
-        echo "Тип дома:" . $this->type . "\n";
-    }
-    //определение количества квартир в подъезде
-    public function kv_pojezd()
-    {
-        $this->kvartir_v_padike = $this->etajey_v_dome * $this->kvartir_na_etaje;
-    }
-    //Расчет № подъезда и этажа
-    public function etajPodjezd()
-    {
-        $result =(($this->nomer_kvartiri - 1)/($this->kvartir_v_padike))+1;//Делим номер квартиры минус 1 на произведение этажности дома и кол-ва квартир на этаже.
-        echo "Подъезд:".$this->podjezd  = floor($result)."\n"; // Целая часть полученного числа + 1 = номер подъезда.
-        echo "Этаж:".$this->etaj = floor(($result - $this->podjezd)*$this->etajey_v_dome)+1 ."\n"; //Дробную часть умножаем на кол-во этажей. Целая часть полученного числа плюс 1 = номер этажа.
+    public function vuvod() {
+        echo "Nuznui podiezd:  " . $this->n_pod . "\n";
+        echo "Nuznui etaz:  " . $this->n_etaz . "\n";
     }
 }
-$obj = new House('KHR_5_4');
-$obj->type();
-$obj->start();
-$obj->kv_pojezd();
-$obj->etajPodjezd();
-echo "\n\n";
-$obj2 = new House('SLT_9_7');
-$obj2->type();
-$obj2->start();
-$obj2->kv_pojezd();
-$obj2->etajPodjezd();*/
-
-$obj = new House();
-$obj->schot();
+ 
+$dom = new House();
+$dom->schot();
+$dom->vuvod();
