523b78070529c4cc4803e214a315fae8e87f1885
Hryshchuk Bohdan <bohdanuk16@gmail.com>
2017-03-03 02:51:32 +0200

    add_k

--- test-blog/models/articles.php
+++ test-blog/models/articles.php
@@ -187,12 +187,14 @@ function articles_new($link, $post_title, $post_text, $tag_name) {
     }
     
     $post = "insert into post (post_title, post_text, post_create_datetime) value ('%s', '%s', now())";
-        
+
     $query_p = sprintf($post, mysqli_real_escape_string($link, $post_title), mysqli_real_escape_string($link, $post_text));
+
     $result_p = mysqli_query($link, $query_p);
-    
+        
     if (!$result_p)
         die(mysqli_error($link));
+
     
     $qwery_t = "select * from tag";
     $result_t = mysqli_query($link, $qwery_t);
@@ -201,87 +203,28 @@ function articles_new($link, $post_title, $post_text, $tag_name) {
         die(mysqli_error($link));
     
     $n_rows = mysqli_num_rows($result_t);    
-    $articles = array();
-    
+       
     for ($i=0; $i < $n_rows; $i++)
     {
-        $row = mysqli_fetch_assoc($result_t);
-        
-        //$tag_name = explode(",", $row[tag_name]);
-        //$tag_id = explode(",", $row[tag_id]);
-        //$tag_id_name = array_combine($tag_id, $tag_name);
-
-        //$row[tag_id_name] = $tag_id_name;
-        
+        $row = mysqli_fetch_assoc($result_t);     
         $tags[] = $row;
     }
     
     $tag_name_prihod = explode(",", $tag_name);
-    array_map(trim, $tag_name_prihod);
-    print_r($tag_name);
-    print_r($tag_name_prihod);
-    print_r($tags);
+    $tag_name_prihod = array_map(trim, $tag_name_prihod);
 
 
+    $query_id_p = "SELECT 
+                        MAX(post_id) 
+                    FROM post" ;
 
-
-
-
-
-$query = "select 
-                post_id,
-          from
-                post
-          limit 1" ;
-    } else {
-     $query = "select 
-                        post.post_id,
-                        post.post_title,
-                        post.post_text,
-                        post.post_create_datetime,
-                        tag.tag_title,
-                        count(post.post_id) as tag_n,
-                        group_concat(tag.tag_title) as tag_name,
-                        group_concat(tag.tag_id) as tag_id
-                        
-                    from
-                        post
-                        left join post_to_tag on (post.post_id=post_to_tag.post_id)
-                            left join tag on (post_to_tag.tag_id=tag.tag_id)
-                    group by
-                        post.post_id" ;
-        
-    }
-    $result = mysqli_query($link, $query);
+    $result_id = mysqli_query($link, $query_id_p);
            
-    if (!$result)
+    if (!$result_id)
         die(mysqli_error($link));
-    
-    $n_rows = mysqli_num_rows($result);
-    
-    $articles = array();
-    
-    for ($i=0; $i < $n_rows; $i++)
-    {
-        $row = mysqli_fetch_assoc($result);
-        
-        $tag_name = explode(",", $row[tag_name]);
-        $tag_id = explode(",", $row[tag_id]);
-        $tag_id_name = array_combine($tag_id, $tag_name);
 
-        $row[tag_id_name] = $tag_id_name;
-        
-        $articles[] = $row;
-    }
-
-
-
-
-
-
-
-
-
+    $post_id = mysqli_fetch_assoc($result_id);
+    $post_id = $post_id['MAX(post_id)'];
 
 
     foreach ($tags as $tag) {
@@ -289,10 +232,67 @@ $query = "select
         foreach ($tag_name_prihod as $tag_n) {
             //echo $tag_n;
             if ($tag['tag_title'] == $tag_n) {
-                echo $tag['tag_id'];
-            } else echo "------" . '/n';
+                $query_in = "INSERT INTO 
+                                        post_to_tag (post_id, tag_id) 
+                            VALUES 
+                                        (" . $post_id . ", " . $tag['tag_id'] . ")" ;
+
+                mysqli_query($link, $query_in);
+            }
         }
     }
+
+    foreach ($tags as $tag) {
+        $tags_new[] = $tag['tag_title'];
+    }
+
+    $tags_new_diff = array_diff($tag_name_prihod, $tags_new);
+
+    $i = null;
+    foreach ($tags_new_diff as $tag) {
+        $query_in = "INSERT INTO 
+                            tag (tag_title) 
+                    VALUES 
+                            ('" . $tag  . "')" ;
+
+        mysqli_query($link, $query_in);
+        $i++;
+    }
+
+    $query_li = "SELECT 
+                    tag_id 
+                FROM 
+                    tag 
+                ORDER BY 
+                    tag_id DESC 
+                LIMIT " . $i ;
+
+    $result_li = mysqli_query($link, $query_li);
+          
+    if (!$result_li)
+        die(mysqli_error($link));
+
+    $n_rows = mysqli_num_rows($result_li);
+    $tag_id_to_post = array();
+    
+    for ($i=0; $i < $n_rows; $i++)
+    {
+        $row = mysqli_fetch_assoc($result_li);
+        $tag_id_to_post[] = $row['tag_id'];
+    }
+
+    $query_in = "INSERT INTO 
+                        post_to_tag (post_id, tag_id) 
+                VALUES 
+                        (" ;
+
+    foreach ($tag_id_to_post as $value) {
+        $query_in .= $post_id . ", " . $value . "), (";
+    
+    }
+
+    $query_in = substr($query_in, 0, -3);
+    mysqli_query($link, $query_in);
     
     return true;    
 }
